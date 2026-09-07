981cbc360b890d17050defd2afd48674d3e7c233
Avishka Akalanka <avishkabiz123@gmail.com>
2026-09-07 13:31:37 +0530

    feat(repository): add Spring Data JPA repositories for Product and Order

---
+++ src/main/java/com/avishka/FlashSale/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.avishka.FlashSale.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.avishka.FlashSale.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+}
