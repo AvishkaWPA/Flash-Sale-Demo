3d112026b7bf06ac764df9b89e5606b3befc1b85
Avishka Akalanka <avishkabiz123@gmail.com>
2026-09-11 13:19:02 +0530

    feat(config): add support for Redis Cloud authentication and SSL/TLS connections

--- src/main/java/com/avishka/FlashSale/config/RedisConfig.java
+++ src/main/java/com/avishka/FlashSale/config/RedisConfig.java
@@ -16,15 +16,27 @@ public class RedisConfig {
     @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.ssl:false}")
+    private boolean useSsl;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer()
-              .setAddress("redis://" + redisHost + ":" + redisPort)
-              .setConnectionMinimumIdleSize(10)
-              .setConnectionPoolSize(64)
+        String protocol = useSsl ? "rediss://" : "redis://";
+
+        var singleServerConfig = config.useSingleServer()
+              .setAddress(protocol + redisHost + ":" + redisPort)
+              .setConnectionMinimumIdleSize(5)
+              .setConnectionPoolSize(32)
               .setConnectTimeout(10000)
               .setTimeout(3000);
+
+        if (redisPassword != null && !redisPassword.isBlank()) {
+            singleServerConfig.setPassword(redisPassword);
+        }
 
         return Redisson.create(config);
     }
